04dfc22369fe8d9d92568db63cdca5e46ce2c7b3
SmartEntity <68695760+SmartEntity@users.noreply.github.com>
2024-12-07 19:57:00 -0500

    MeepMeep paths

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -11,26 +11,80 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(750);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity left = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(48, -48, Math.toRadians(180)))
-                        //go to sub
-                        .forward(96)
-                        .turn(Math.toRadians(-90))
-                        .forward(96)
-                        .turn(Math.toRadians(-90))
-                        .forward(96)
-                        .turn(Math.toRadians(-90))
-                        .forward(96)
-                        .turn(Math.toRadians(-90))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-12, -72+9, Math.toRadians(270)))
+                        //preload
+                        .setTangent(Math.toRadians(90))
+                        .splineToConstantHeading(new Vector2d(-10,-24-8),Math.toRadians(90))
+                        //deliver 1
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(-38,-30,Math.toRadians(135)),Math.toRadians(135))
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(-54,-54,Math.toRadians(225)),Math.toRadians(225))
+                        //deliver 2
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(-50,-30,Math.toRadians(135)),Math.toRadians(135))
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(-54,-54,Math.toRadians(225)),Math.toRadians(225))
+                        //deliver 3
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(-56,-24,Math.toRadians(180)),Math.toRadians(180))
+                        .setTangent(Math.toRadians(-45))
+                        .splineToLinearHeading(new Pose2d(-54,-54,Math.toRadians(225)),Math.toRadians(225))
+                        //deliver 4
+                        .setTangent(Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(-10,-24-8,Math.toRadians(-90)),Math.toRadians(90))
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(-54,-54,Math.toRadians(225)),Math.toRadians(225))
+                        //park
+                        .setTangent(Math.toRadians(60))
+                        .splineToLinearHeading(new Pose2d(-30,-12,Math.toRadians(-90)),Math.toRadians(90))
+                        .build());
+
+        RoadRunnerBotEntity right = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 18)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(12, -72+9, Math.toRadians(270)))
+                        //preload
+                        .setTangent(Math.toRadians(90))
+                        .splineToConstantHeading(new Vector2d(10,-24-8),Math.toRadians(90))
+                        //deliver 1
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(38,-30,Math.toRadians(45)),Math.toRadians(45))
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(48,-48,Math.toRadians(-90)),Math.toRadians(-90))
+                        .splineToConstantHeading(new Vector2d(48,-60),Math.toRadians(-90))
+                        .setTangent(Math.toRadians(90))
+                        .splineToConstantHeading(new Vector2d(10,-24-8),Math.toRadians(90))
+                        //deliver 2
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(50,-30,Math.toRadians(45)),Math.toRadians(45))
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(48,-48,Math.toRadians(-90)),Math.toRadians(-90))
+                        .splineToConstantHeading(new Vector2d(48,-60),Math.toRadians(-90))
+                        .setTangent(Math.toRadians(90))
+                        .splineToConstantHeading(new Vector2d(10,-24-8),Math.toRadians(90))
+                        //deliver 3
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(56,-24,Math.toRadians(0)),Math.toRadians(0))
+                        .setTangent(Math.toRadians(-90))
+                        .splineToLinearHeading(new Pose2d(48,-48,Math.toRadians(-90)),Math.toRadians(-90))
+                        .splineToConstantHeading(new Vector2d(48,-60),Math.toRadians(-90))
+                        .setTangent(Math.toRadians(90))
+                        .splineToConstantHeading(new Vector2d(10,-24-8),Math.toRadians(90))
+                        //park
+                        .setTangent(Math.toRadians(-90))
+                        .splineToConstantHeading(new Vector2d(36,-60),Math.toRadians(-90))
                         .build());
 
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(left)
+                .addEntity(right)
                 .start();
     }
 }
