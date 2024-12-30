3040ed08acef9ff1dfe8f250fab2c74c8e4a4e46
SmartEntity <68695760+SmartEntity@users.noreply.github.com>
2024-12-30 14:59:15 -0500

    Servo tester

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bareBones/servoTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bareBones/servoTester.java
@@ -3,29 +3,43 @@ package org.firstinspires.ftc.teamcode.bareBones;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.game.Controller;
 
 @TeleOp(group = "Barebone Component Testing")
 public class servoTester extends OpMode {
-    public CRServo right;
-    public CRServo left;
+    public Servo arm;
+    public Servo linkage;
+
+    public Controller controller;
+
+    double linkagePos = 0, armPos = 0;
 
     @Override
     public void init() {
-        right = hardwareMap.crservo.get("RightIntake");
-        left = hardwareMap.crservo.get("LeftIntake");
+        arm = hardwareMap.servo.get("ArmRotation");
+        linkage = hardwareMap.servo.get("Linkage");
+
+        arm.setPosition(armPos);
+        linkage.setPosition(linkagePos);
     }
 
     @Override
     public void loop() {
-        if(gamepad1.square) {
-            right.setPower(-1);
-            left.setPower(1);
-        }else if(gamepad1.circle) {
-            right.setPower(1);
-            left.setPower(-1);
-        }else if(gamepad1.triangle) {
-            right.setPower(0);
-            left.setPower(0);
+        if(controller.isPressed(Controller.Button.DPAD_UP)){
+            armPos += 0.05;
+        }else if (controller.isPressed(Controller.Button.DPAD_DOWN)){
+            armPos -= 0.05;
         }
+
+        if(controller.isPressed(Controller.Button.DPAD_RIGHT)){
+            linkagePos += 0.05;
+        }else if (controller.isPressed(Controller.Button.DPAD_LEFT)){
+            linkagePos -= 0.05;
+        }
+
+        arm.setPosition(armPos);
+        linkage.setPosition(linkagePos);
     }
 }
