023e5820e885fdf4d92e332160c4259398d80db9
SmartEntity <68695760+SmartEntity@users.noreply.github.com>
2025-02-03 19:31:12 -0500

    wiuenveiurvniweu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MVTest.java
@@ -1,4 +1,42 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-public class MVTest {
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.components.BaseComponent;
+import org.firstinspires.ftc.teamcode.components.MachineVisionSubmersible;
+import org.firstinspires.ftc.teamcode.components.RobotContext;
+import org.firstinspires.ftc.teamcode.game.Controller;
+
+public class MVTest extends OpMode {
+
+    protected Controller driver;
+    RobotContext robotContext;
+    MachineVisionSubmersible mvs;
+
+    Pose2d lastElemPos;
+
+    @Override
+    public void init() {
+        driver = new Controller(gamepad1);
+        robotContext = BaseComponent.createRobotContext(this);
+        mvs = new MachineVisionSubmersible(robotContext);
+
+        mvs.init();
+    }
+
+    @Override
+    public void loop() {
+
+        if(driver.isPressed(Controller.Button.CROSS)){
+
+            lastElemPos = mvs.runPipeline();
+            telemetry.addData("last element position: ", lastElemPos);
+        }
+
+
+
+        telemetry.update();
+
+    }
 }
